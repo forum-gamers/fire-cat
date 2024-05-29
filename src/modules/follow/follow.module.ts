db1df9829853cb900b4cd30ccfb8d9f16008fa2c
anandafiqri <anandafiqri1403@gmail.com>
2024-05-29 08:46:47 +0700

    fix:import user service provider

--- src/modules/follow/follow.module.ts
+++ src/modules/follow/follow.module.ts
@@ -5,10 +5,11 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Follow } from '../../models/follow';
 import { User } from '../../models/user';
 import { FollowController } from '../../controllers/follow.controller';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Follow, User])],
-  providers: [FollowService, FollowValidation],
+  providers: [FollowService, FollowValidation, UserService],
   controllers: [FollowController],
 })
 export class FollowModule {}
